38efee39eee4253916a4a479fcf4d1853ee94bbe
anorimaki <anorimaki@gmail.com>
2016-08-09 23:01:08 +0200

    - Corregida configuración del ancho de pulso. Se podía sobrepasar el mínimo.

--- piccode/include/mathutil.h
+++ piccode/include/mathutil.h
@@ -2,7 +2,7 @@
 #define	MATHUTIL_H
 
 #define CHECKED_ADD(a, b, max) ((max-a)>b ? a+b : max)
-#define CHECKED_SUB(a, b, min) ((min+a)<b ? min : a-b)
+#define CHECKED_SUB(a, b, min) ((a-min)>b ? a-b : min)
 
 signed int32 math_change_range(signed int16 value, int16 max_value, 
 								int16 new_max_value);
